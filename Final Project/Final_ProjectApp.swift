e0329a9ca9c66cca97edf655ca0a15af4500e90e
mileyarora <miley@abris.io>
2024-07-26 13:22:30 -0400

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Miley Arora on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
